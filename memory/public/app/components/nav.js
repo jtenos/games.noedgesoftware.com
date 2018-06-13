2c9bac063fc1bf66023f698a6af912e43d750380
Joe Enos <jtenos@outlook.com>
2018-06-12 21:14:25 -0700

    Starting to add react

---
+++ memory/public/app/components/nav.js
@@ -0,0 +1,41 @@
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Memory</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul id="nav-list" class="nav navbar-nav">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="one-player-dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                One Player
+              </a>
+              <div class="dropdown-menu" aria-labelledby="one-player-dropdown">
+                <a class="dropdown-item" href="#">Easy (3x4)</a>
+                <a class="dropdown-item" href="#">Medium (4x6)</a>
+                <a class="dropdown-item" href="#">Hard (6x8)</a>
+              </div>
+            </li>
+             <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+            <!--<li><a href="#about">Player vs. Computer</a></li>-->
+            <!--<li><a href="#contact">Two Player - Same Device</a></li>-->
+            <!--<li><a href="#contact">Two Player - Remote</a></li>-->
+          </ul>
+        </div>
+      </div>
+    </nav>
